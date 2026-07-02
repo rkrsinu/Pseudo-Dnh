a4e0fa984dae74d44a9dd4befb53636f0057049e
rkrsinu <152066159+rkrsinu@users.noreply.github.com>
2026-07-02 15:02:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -632,12 +632,14 @@ if st.button("Predict"):
         f"Tor (τref = 100 s): "
         f"{Tor:.2f} ± {Tor_err:.2f} K"
     )
-    st.markdown("**Formula:**")
-
-    st.latex(r"""
+    st.markdown(r"""
+    <div style="text-align:left;">
+    \[
     T_{\mathrm{Or}}
     =
     -\frac{U_{\mathrm{eff}}}
     {\ln\left(\frac{\tau_{0}}
     {\tau_{\mathrm{ref}}}\right)}
-    """)
+    \]
+    </div>
+    """, unsafe_allow_html=True)
